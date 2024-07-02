79e0c69355bd25ddcaffc25886a968addaa3f5e2
farhang-sa <Saeedi.Farhang@gmail.com>
2024-07-02 10:05:05 +0330

    ui changed - renaming/deleting files - download quality added

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ App itself is easy as it gets! but mixing was good.
 ## How it works ( for dev ) :
 <code>nodemon.js</code> is the answer. <code>npm start</code> launches the <code>nodemon.js</code>
 and it will execute an empty <code>fake-server-script.js</code>. ( as we need php server not js server )
-after that , it starts a <code>php-built-in-server [php -S localhost:8080 -t ./dist]</code> for serving files. so nodemon watches the files for changes ( to run webpack ) and php servs them.
+after that , it starts a <code>php-built-in-server [php -S 0.0.0.0:8080 -t ./dist]</code> for serving files. so nodemon watches the files for changes ( to run webpack ) and php servs them.
 
 ## How to use :
 1. <code>git clone</code> to your server
@@ -16,4 +16,4 @@ after that , it starts a <code>php-built-in-server [php -S localhost:8080 -t ./d
 3. delete dev files and keep the product :
    - if server is windows : <code>npm run windows</code>
    - if server is linux : <code>npm run linux</code>
-4. upload it any where you want and use a REAL php server
+4. or just use releases :)
--- development/scripts/app.js
+++ development/scripts/app.js
@@ -3,11 +3,53 @@ import { createRoot } from "react-dom/client";
 import { BsCloudDownloadFill } from "react-icons/bs";
 import { BsTrash3Fill } from "react-icons/bs";
 import { BsListCheck } from "react-icons/bs";
+import { BsPencilFill } from "react-icons/bs";
+import { BsFillFolderFill } from "react-icons/bs";
 import ReactLoading from 'react-loading';
 import $ from 'jquery' ;
+import '../styles/app.scss';
 
 window.React = React;
 window.$ = $ ;
+window.baseName = typeof window.baseName !== "undefined" ? window.baseName : '/' ;
+
+const findExt = ( fileName ) =>{
+	if( typeof fileName !== 'string' )
+		return 'mp4' ;
+	if( fileName.endsWith('.webm' ) )
+		return 'webm' ;
+	return 'mp4' ;
+}
+
+const findGoodName = ( fileName ) =>{
+	let filExt = findExt( fileName );
+	let newName = fileName ;
+	newName = newName.replaceAll( '.' + filExt , '' );
+	if( newName.length > 40 )
+		newName = newName.slice(0, 40 );
+	newName = newName.replaceAll( ' ' , '_' );
+	newName = newName.replace( /[^a-zA-Z0-9_.-]/g , '' ); // clean name
+	newName = newName.trim( ' .' );
+	return newName + '.' + filExt ;
+}
+
+const getIcons = () => {
+	return {
+		icoTrash : $( 'div.icon-holder-trash' ).html() ,
+		icoDownload : $( 'div.icon-holder-download' ).html() ,
+		icoRename : $( 'div.icon-holder-rename' ).html() ,
+	}
+}
+
+const getFileView = ( fileName ) => {
+	let { icoDownload , icoRename , icoTrash } = getIcons();
+	let view =  '<div class="m-3 text-start">';
+	view += `<span class='btn btn-md btn-danger' title="delete" onclick="window.askYtdlDelete('${fileName}');">${icoTrash}</span> `;
+	view += `<span class='btn btn-md btn-info' title="rename" onclick="window.askYtdlRename('${fileName}');">${icoRename}</span> `;
+	view += `<a class="btn" href='${window.baseName + fileName}' download><span class='btn btn-md btn-primary'>${icoDownload}</span> ${fileName}</a>`;
+	view += "</div>" ;
+	return view ;
+}
 
 const App = () => {
 
@@ -17,7 +59,10 @@ const App = () => {
 	const winBinLink = 'http://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe' ;
 	const linBinLink = 'http://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux' ;
 
-	window.askDelete = ( fileName ) =>{
+	window.askYtdlDelete = (fileName ) =>{
+		let ask = confirm(`Are you sure you want to delete '${fileName}'?` );
+		if( ! ask )
+			return ;
 		setSystemMessage( 'deleting \'' + fileName + '\'' );
 		setIsDownloading( true );
 		$.post( window.baseName + "api.php", {
@@ -28,11 +73,48 @@ const App = () => {
 		});
 	}
 
-	const askDownload = ( action ) => {
+	window.askYtdlRename = (fileName ) =>{
+		let filExt = findExt( fileName );
+		let newName = findGoodName( fileName );
+		newName = prompt( 'Enter new name with .extension' , newName );
+		if( ! newName )
+			return ;
+		setSystemMessage( 'renaming \'' + fileName + '\'' );
 		setIsDownloading( true );
-		let link = action ? action : $( 'input.link-input' ).val();
+		// clean name again!
+		newName = newName.replaceAll( ' ' , '_' );
+		newName = newName.replace( /[^a-zA-Z0-9_.-]/g , '' );
+		if( ! newName.endsWith( filExt ) ){
+			newName = newName.replaceAll( filExt , '' );
+			newName = newName + '.' + filExt ;
+		} if( newName.length === 0 )
+			newName = findGoodName( fileName );
+		else if( newName === filExt )
+			newName = findGoodName( fileName );
+		else if( newName.startsWith( '.' ) )
+			newName = findGoodName( fileName );
 		$.post( window.baseName + "api.php", {
-			'link' : link
+			'link' : 'renamefile:' + fileName ,
+			'to' : newName
+		} , function( response ) {
+			let newName = response.replace( 'video renamed:' , '' );
+			response = getFileView( newName );
+			setSystemMessage( response );
+			setIsDownloading( false );
+		});
+	}
+
+	const askDownload = ( action ) => {
+		if( action === 'clean' || action === 'clear' ){
+			let ask = confirm(`Are you sure you want to delete all video files?` );
+			if( ! ask )
+				return ;
+		} // resume :
+		let link = action ? action : $( 'input.link-input' ).val();
+		setIsDownloading( true );
+		$.post( window.baseName + "api.php", {
+			'link' : link ,
+			'quality' : $( 'select#quality' ).val()
 		} , function( response ) {
 			if( response === 'yt-dlp binary not prepared' ){
 				response = 'yt-dlp binary files not available ( check internet )';
@@ -52,15 +134,8 @@ const App = () => {
 					let html = $( "<div></div>" );
 					responseValues = JSON.parse( responseValues );
 					let co = 0 ;
-					let icoTrash = $( 'div.icon-holder-trash' ).html();
-					let icoDownload = $( 'div.icon-holder-download' ).html();
-					for( let video in responseValues ){
-						let nh = '<div class="mt-1 text-start">' + (++co ) + ". " + video + ' ';
-						nh += `<a class='btn btn-md btn-primary' href='${video}' download>${icoDownload}</a> `;
-						nh += `<span class='btn btn-md btn-danger' onclick="window.askDelete('${video}');">${icoTrash}</span><br /> `;
-						nh += "</div>" ;
-						html.append( nh );
-					}
+					for( let video in responseValues )
+						html.append( getFileView( video ) );
 					response = html.html() ;
 				}
 			} else { // File Name received !
@@ -78,41 +153,38 @@ const App = () => {
 	}
 
 	return (
-		<>
-			<div className='icon-holder-trash hidden' style={{'display':'none'}}>
+		<div className="container">
+			<div className='icon-holder-trash hidden'>
 				<BsTrash3Fill/>
 			</div>
-			<div className='icon-holder-download hidden' style={{'display':'none'}}>
+			<div className='icon-holder-download hidden'>
 				<BsCloudDownloadFill/>
 			</div>
-			<div className="container">
-				<header><h4>Welcome To YouTube Downloader</h4></header>
-				<div className="row mt-5">
-					<div className="col-12">
+			<div className='icon-holder-rename hidden'>
+				<BsPencilFill />
+			</div>
+			<div className="row">
+				<div className="col-12 col-sm-2"></div>
+				<div className="col-12 col-sm-8">
+					<header className="col-12">
+						<h4>Welcome To YouTube Downloader</h4>
+					</header>
+					<div className="col-12 mt-5">
 						<h5>
 							See this &#160;
 							<a target="_blank"
 							   href="https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md">link
 							</a>&#160; for supported sites
-							<br/><br/>
-							<BsListCheck/> list all videos - <BsTrash3Fill/> delete all videos
 						</h5>
 					</div>
-				</div>
-				<div className="row mt-4">
-					<div className="col-12 col-sm-2"></div>
-					<div className="col-12 col-sm-8">
+					<div className="col-12 mt-5">
 						<span className="input-group">
-							<span className="input-group-addon p-4">
-								<BsListCheck
-									onClick={() => askDownload('list')}
-									style={iconsStyle}/>
-							</span>
-							<span className="input-group-addon p-4">
-								<BsTrash3Fill
-									onClick={() => askDownload('clean')}
-									style={iconsStyle}/>
-							</span>
+							<select className="select me-2 ps-2 pe-2" id='quality'>
+								<option value='360'>Quality 360p</option>
+								<option value='480' selected="1">Quality 480p</option>
+								<option value='720'>Quality 720p</option>
+								<option value='1080'>Quality 1080p</option>
+							</select>
 							<input type="text"
 								   className="form-control form-control-lg link-input"
 								   placeholder="Enter link"/>
@@ -127,25 +199,37 @@ const App = () => {
 							</span>)}
 						</span>
 					</div>
-				</div>
-				{systemMessage.length > 0 ? (<div className="row p-4 mt-4 p-0">
-					<div className="col-12 col-sm-2 p-0"></div>
-					<div className="col-12 col-sm-8 p-0">
+					<div className="col-12 mt-5">
+						<span className="btn btn-success p-3"
+							  title="list all video files" style={{cursor:"pointer"}}
+							  onClick={() => askDownload('list')}>
+							<BsFillFolderFill style={iconsStyle}/> List All
+						</span>
+						<span className="btn btn-danger p-3"
+							  onClick={() => askDownload('clean')}
+							  title="Delete all video files" style={{cursor:"pointer"}}>
+							<BsTrash3Fill style={iconsStyle}/> Delete All
+						</span>
+					</div>
+					{systemMessage.length > 0 ? (<div className="col-12 mt-4 pt-4">
 						<div className="alert alert-info system-message">
 							<h5 className="m-0" dangerouslySetInnerHTML={{__html: systemMessage}}></h5>
 						</div>
-					</div>
-				</div>) : (<div></div>)}
-				<footer className="mt-5">
-					<h5>Note : if you are in dev mode and have low speed internet ( like me ) <br />
-						download yt-dlp binary from <a href={winBinLink} download>windows-link</a>&#160; or &#160;
-						<a href={linBinLink} download>linux-link</a> and put it beside api
-					</h5>
-					<br />
-					<h5>All rights NOT reserved for <a href='https://github.com/farhang-sa' target="_blank">ME</a> </h5>
-				</footer>
+					</div>) : (<div></div>)}
+					<footer className="col-12 mt-5 pt-2">
+						<h5 className="alert alert-info">
+							Note : if you are in dev mode and have low speed internet ( like me )
+							<br /> download a yt-dlp binary from&#160;
+							<a className="btn btn-lg btn-primary" href={winBinLink} download>windows-link</a>
+							&#160; or &#160;
+							<a className="btn btn-lg btn-primary" href={linBinLink} download>linux-link</a> and put it beside api
+						</h5>
+						<br />
+						<h5>All rights are NOT reserved for <a href='https://github.com/farhang-sa' target="_blank">ME</a> </h5>
+					</footer>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
